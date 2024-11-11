7bd224bc139fcb1e99782cf841fa13427194445b
Jo Matsumaru <hatunehimeka@outlook.jp>
2024-11-12 00:41:01 +0900

    Windows向けのメモリ管理機能を追加し、コマンド引数の構造を改善

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use std::env::consts::OS;
 use sysinfo::{Components, Disks, Networks, System};
 
 #[derive(Parser)]
@@ -10,31 +11,61 @@ struct Args {
 #[derive(Subcommand)]
 enum Commands {
     Mem {
+        #[command(subcommand)]
+        action: Option<MemShowCommands>,
+    },
+}
+
+#[derive(Subcommand)]
+enum MemShowCommands {
+    Show {
         #[arg(short, long)]
-        show: bool,
+        all: bool,
         #[arg(short, long)]
-        clear: bool,
+        free: bool,
     },
 }
 fn main() {
     let args = Args::parse();
+
+    match OS {
+        "windows" => {
+            windows_cmd(args);
+        }
+        _ => {
+            println!("Unsupported OS");
+        }
+    }
+}
+
+fn windows_cmd(args: Args) {
     match &args.command {
-        Some(Commands::Mem { show, clear }) => {
+        Some(Commands::Mem { action }) => {
             let mut sys = System::new_all();
             sys.refresh_all();
-            if *clear {
-                sys.refresh_memory();
-            }
-            if *show {
-                let showmem = sys.total_memory();
-                println!("Memory: {} KB", &showmem);
-            } else {
-                let mem = sys.total_memory();
-                println!("Memory: {} KB", &mem);
+            match action {
+                Some(MemShowCommands::Show { all, free }) => {
+                    if *all {
+                        let total_memory = sys.total_memory();
+                        println!("Total Memory: {} GB", bytes_to_gb(total_memory));
+                    }
+                    if *free {
+                        let free_memory = sys.free_memory();
+                        println!("Free Memory: {} GB", bytes_to_gb(free_memory));
+                    }
+                }
+                None => {
+                    println!("No action specified for Mem command");
+                }
             }
         }
         None => {
             println!("No subcommand was used");
         }
     }
+}
+
+fn bytes_to_gb(bytes: u64) -> f64 {
+    let gb = bytes as f64 / (1024.0 * 1024.0 * 1024.0);
+    format!("{:.2}", gb).parse().unwrap_or(0.0)
 }
